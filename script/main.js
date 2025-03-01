b8d43f1b7196a023668011f480501ac046449db6
ramimahammadarpon <ramim6708@gmail.com>
2025-03-01 19:49:05 +0600

    on page Load realtime date aded

--- script/main.js
+++ script/main.js
@@ -9,7 +9,8 @@ window.onload = function date() {
     const weekDay = ["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"];
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
     console.log(weekDay[week], months[month], day, year);
-    document.getElementById("week-day");
+    setValueById("week-day",weekDay[week]);
+    setValueById("month-day-year",`${months[month]} ${day} ${year}`);
 
 
 }
