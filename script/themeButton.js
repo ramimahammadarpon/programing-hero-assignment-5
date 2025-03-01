f1be70afeb4a2add1167d47220b90111c2ab86f8
ramimahammadarpon <ramim6708@gmail.com>
2025-03-01 20:21:25 +0600

    Functionality of Theme Button Added

--- script/themeButton.js
+++ script/themeButton.js
@@ -1,3 +1,8 @@
-document.getElementById("theme-btn").addEventListener("click",function() {
-
+document.getElementById("theme-btn").addEventListener("click",function(event) {
+    event.preventDefault();
+    console.log("you're inside my bro");
+    const colors = ["#ffcece","#fec5bb","#a39e92","#fa9bcf","#a9e4de","#d3dce8","#ffcad4","#faedcb","#e7f1dc","#e3e7e0","#a99abd"];
+    const random = Math.floor(Math.random()*11);
+    console.log(random);
+    document.getElementById("body").style.backgroundColor = colors[random];
 })
